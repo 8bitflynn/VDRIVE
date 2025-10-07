895997d87ef88e405600f2ea54a060e3837a570e
8bitflynn <8bitflynn@LAPTOP-HD0LVV07>
2025-10-07 09:54:59 -0500

    Fixed project

--- VDRIVE/Contracts/ILoad.cs
+++ VDRIVE/Contracts/ILoad.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using VDRIVE.Structures;
+﻿using VDRIVE_Contracts.Structures;
 
 namespace VDRIVE.Contracts
 {
